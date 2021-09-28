ceba7aa7bdae327218b1a4b38bb052f4b265bd7e
h-a-n-a <andywangsy@gmail.com>
2021-09-28 20:50:46 +0800

    feat: implement parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::cell::{Cell, RefCell};
+use std::cell::RefCell;
 
 #[derive(Debug, PartialEq)]
 enum TokenKind {
@@ -22,7 +22,7 @@ fn tokenizer(input: &str) -> Vec<Token> {
     let chars = input.chars().collect::<Vec<char>>();
 
     while current < chars.len() {
-        if let Some(c) = chars.get(current) {
+        if let Some(&c) = chars.get(current) {
             match c {
                 '(' | ')' => {
                     tokens.push(Token {
@@ -31,17 +31,27 @@ fn tokenizer(input: &str) -> Vec<Token> {
                     });
                 }
                 '0'..='9' => {
-                    tokens.push(Token {
-                        kind: TokenKind::Number,
-                        value: c.to_string(),
-                    });
-                }
-                _ => {
-                    if *c == ' ' {
-                        current += 1;
-                        continue;
+                    let mut value = c.to_string();
+
+                    while let Some(&next_char) = chars.get(current + 1) {
+                        match next_char {
+                            '0'..='9' => {
+                                value = format!("{}{}", value, next_char);
+                                current += 1;
+                            }
+                            _ => break,
+                        }
                     }
 
+                    tokens.push(Token {
+                        kind: TokenKind::Number,
+                        value,
+                    });
+                }
+                ' ' => {
+                    // skip whitespaces
+                }
+                'a'..='z' | 'A'..='Z' => {
                     let mut value = c.to_string();
                     while let Some(&next_char) = chars.get(current + 1) {
                         match next_char {
@@ -58,6 +68,9 @@ fn tokenizer(input: &str) -> Vec<Token> {
                         value,
                     })
                 }
+                _ => {
+                    panic!("[tokenizer]: unexpected token {}", c)
+                }
             }
         }
 
@@ -67,7 +80,7 @@ fn tokenizer(input: &str) -> Vec<Token> {
     tokens
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum NodeKind {
     Program,
     CallExpression,
@@ -77,14 +90,15 @@ enum NodeKind {
 #[derive(Debug)]
 struct Program {
     kind: NodeKind,
-    body: RefCell<Vec<Node>>,
+    body: RefCell<Vec<RefCell<Node>>>,
 }
 
 #[derive(Debug, Clone)]
 struct Node {
     kind: NodeKind,
-    name: String,
-    params: Vec<Box<Node>>,
+    name: Option<String>,
+    value: Option<String>,
+    params: Option<RefCell<Vec<RefCell<Node>>>>,
 }
 
 /*
@@ -99,30 +113,122 @@ fn parser(tokens: &Vec<Token>) -> Program {
         body: RefCell::new(vec![]),
     };
 
-    let ref mut working_node: Option<&Node> = None;
+    let mut current = 0usize;
 
-    // for (index, token) in tokens.iter().enumerate() {
-    //     if token.kind == TokenKind::Paren && token.value == '('.to_string() {
-    //         let node = Node {
-    //             kind: NodeKind::CallExpression,
-    //             name: token.value.to_owned(),
-    //             params: &mut vec![],
-    //         };
+    fn walk(current: &mut usize, tokens: &Vec<Token>) -> RefCell<Node> {
+        let token = &tokens[*current];
+
+        return match token.kind {
+            TokenKind::Paren => {
+                if token.value == "(".to_string() {
+                    *current += 1;
+                    let name_token = &tokens[*current];
+
+                    let mut params: Vec<RefCell<Node>> = vec![];
+
+                    *current += 1;
+                    let mut current_token = &tokens[*current];
+
+                    while (current_token.kind != TokenKind::Paren)
+                        || (current_token.kind == TokenKind::Paren
+                            && current_token.value != ")".to_string())
+                    {
+                        params.push(walk(current, &tokens));
+                        current_token = &tokens[*current];
+                    }
+
+                    *current += 1;
+
+                    return RefCell::new(Node {
+                        kind: NodeKind::CallExpression,
+                        name: Some(name_token.value.to_owned()),
+                        value: None,
+                        params: Some(RefCell::new(params)),
+                    });
+                }
+
+                panic!("[parser]: unmatched token {}", token.value);
+            }
+            TokenKind::Name => {
+                *current += 1;
+                RefCell::new(Node {
+                    kind: NodeKind::NumberLiteral,
+                    name: None,
+                    value: Some(token.value.to_owned()),
+                    params: None,
+                })
+            }
+            TokenKind::Number => {
+                *current += 1;
+                RefCell::new(Node {
+                    kind: NodeKind::NumberLiteral,
+                    name: None,
+                    value: Some(token.value.to_owned()),
+                    params: None,
+                })
+            }
+        };
+    }
+
+    while current < tokens.len() {
+        ast.body.borrow_mut().push(walk(&mut current, &tokens));
+    }
+
+    // let mut stack: Vec<Rc<RefCell<Node>>> = vec![];
     //
-    //         // *working_node = ;
+    // for Token { kind, value } in tokens.iter() {
+    //     match *kind {
+    //         TokenKind::Paren => {
+    //             match value.as_str() {
+    //                 "(" => {
+    //                     let node = Rc::new(RefCell::new(Node {
+    //                         kind: NodeKind::CallExpression,
+    //                         name: Some(String::default()),
+    //                         value: None,
+    //                         params: Some(RefCell::new(vec![])),
+    //                     }));
+    //
+    //                     if stack.len() == 0 {
+    //                         ast.body.borrow_mut().push(Rc::clone(&node));
+    //                     } else {
+    //                         if let Some(last) = stack.last() {
+    //                             if let Some(ref params) = last.borrow_mut().params {
+    //                                 params.borrow_mut().push(Rc::clone(&node))
+    //                             }
+    //                         }
+    //                     }
+    //
+    //                     stack.push(Rc::clone(&node));
+    //                 }
+    //                 ")" => {
+    //                     if stack.len() == 0 {
+    //                         panic!("[parser] grammar error");
+    //                     }
+    //                     stack.pop();
+    //                 }
+    //                 _ => (),
+    //             };
+    //         }
+    //         TokenKind::Name => {
+    //             if let Some(last) = stack.last() {
+    //                 last.borrow_mut().name = Some(value.to_string());
+    //             }
+    //         }
+    //         TokenKind::Number => {
+    //             if let Some(last) = stack.last() {
+    //                 let node = Rc::new(RefCell::new(Node {
+    //                     kind: NodeKind::NumberLiteral,
+    //                     value: Some(value.to_string()),
+    //                     name: None,
+    //                     params: None,
+    //                 }));
+    //                 if let Some(ref params) = last.borrow_mut().params {
+    //                     params.borrow_mut().push(Rc::clone(&node));
+    //                 }
+    //             }
+    //         }
     //     }
     // }
-
-    // let ref mut ref_mut = *ast.body.borrow_mut();
-    //
-    // *ref_mut = vec![Node {
-    //     kind: NodeKind::CallExpression,
-    //     name: "a".to_string(),
-    //     params: vec![],
-    // }]
-    // .to_owned();
-    //
-    // println!("{:?}", tokens);
 
     ast
 }
@@ -132,5 +238,5 @@ fn main() {
 
     let tokens = tokenizer(lisp_input);
     let ast = parser(&tokens);
-    println!("{:?}", ast)
+    println!("{:#?}", ast)
 }
